6c34a3a6c6db828ce1480e91ae528b8e95b14d52
RaquelGomes25 <92063008+RaquelGomes25@users.noreply.github.com>
2021-10-12 16:28:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendendo basico de CSS</title>
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+      </head>
+  <body class="tela-estilizada">
+    Aqui vai o código HTML que fará seu site aparecer.
+  </body>
+</html>
